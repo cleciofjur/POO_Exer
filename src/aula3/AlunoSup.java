fc0256c3e9f588f0b7321eb80bfcba8df703a212
cleciofjur <cleciofjur21@gmail.com>
2026-03-17 10:43:54 -0400

    Adicionando...

--- src/aula3/AlunoSup.java
+++ src/aula3/AlunoSup.java
@@ -1,5 +1,22 @@
 package aula3;
 
-public class AlunoSup extends Aluno{
+public class AlunoSup extends AlunoTec{
+    Double n4;
+    Double tcc;
 
+    void calcMedia () {
+        media = (n1 + n2 + n3 + n4) / 4;
+
+        if (media >= 7) {
+            status = "Aprovado";
+        } else if (media >= 5) {
+            status = "Em exame";
+        } else {
+            status = "Reprovado";
+        }
+    }
+
+    void calcMedia(Double tcc) {
+
+    }
 }

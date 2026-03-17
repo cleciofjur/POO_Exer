193e747b8ade493b9562af2585f333d42d39ccd7
cleciofjur <cleciofjur21@gmail.com>
2026-03-17 10:23:07 -0400

    aula3

--- src/aula3/Veiculo.java
+++ src/aula3/Veiculo.java
@@ -10,9 +10,9 @@ public class Veiculo {
         if (valor <= 50000.0) {
             imposto = valor * 2 / 100;
         } else if (valor <= 80000.0) {
-            imposto = valor * 3 / 100;
+            imposto = valor * 0.03 / 100;
         } else {
-            imposto = valor * 3 / 100;
+            imposto = valor * 0.05 / 100;
         }
     }
 
@@ -20,7 +20,7 @@ public class Veiculo {
         if (marca.equals("BYD")) {
             imposto = valor * 1 / 100;
         } else if ((marca.equals("HONDA")) || (marca.equals("TOYOTA"))) {
-            imposto = valor * 3 / 100;
+            imposto = valor * 0.03 / 100;
         } else {
             imposto = valor * 0.05;
         }

92b0b4f6f1870dc77e31eb5f13961b8eff8cae63
Clecio Júnior <cleciofjur21@gmail.com>
2026-03-24 10:38:43 -0400

    Add Curso class with name and value attributes

---
+++ CursoValor/Curso.java
@@ -0,0 +1,13 @@
+package aula4_2;
+
+public class Curso {
+    String nome;
+    Double valor;
+
+    Curso () {}
+
+        Curso (String nome, Double valor) {
+            this.nome = nome;
+            this.valor = valor;
+        }
+}
